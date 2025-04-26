5f2811a850a97d13525a7d143f561568fc1699e4
Joao Gilberto Saraiva <jgilbertons@gmail.com>
2025-04-26 15:54:10 -0300

    feat: add Post top rated query

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,12 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
   validates :ip, presence: true
+
+  def self.top_rated(limit = 10)
+    Post.select("posts.*, COALESCE(AVG(ratings.value), 0) as average_rating")
+        .left_joins(:ratings)
+        .group("posts.id")
+        .order("average_rating DESC, posts.id")
+        .limit(limit)
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -11,4 +11,32 @@ RSpec.describe Post, type: :model do
     it { should validate_presence_of(:body) }
     it { should validate_presence_of(:ip) }
   end
+
+  describe 'top_rated' do
+    let(:post1) { create(:post) }
+    let(:post2) { create(:post) }
+    let(:post3) { create(:post) }
+    let(:post4) { create(:post) }
+
+
+    before do
+      create(:rating, post: post1, value: 5)
+      create(:rating, post: post2, value: 3)
+      create(:rating, post: post2, value: 4)
+      create(:rating, post: post3, value: 2)
+      post4
+    end
+
+    it 'returns posts ordered by average rating' do
+      expect(Post.top_rated).to eq([ post1, post2, post3, post4 ])
+    end
+
+    it 'returns posts with no ratings' do
+      expect(Post.top_rated.last).to eq(post4)
+    end
+
+    it 'returns posts respecting the limit' do
+      expect(Post.top_rated(2)).to eq([ post1, post2 ])
+    end
+  end
 end
