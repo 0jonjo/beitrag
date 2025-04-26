87bc1f9e32d4e47fa98734301ef635658036f88f
Joao Gilberto Saraiva <jgilbertons@gmail.com>
2025-04-26 09:48:49 -0300

    test: add check if integer to Rating model

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,6 +2,6 @@ class Rating < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  validates :value, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  validates :value, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
   validates :user_id, uniqueness: { scope: :post_id, message: "A post can be rated once" }
 end
--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -9,33 +9,34 @@ RSpec.describe Rating, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:value) }
     it { should validate_numericality_of(:value).is_greater_than_or_equal_to(1).is_less_than_or_equal_to(5) }
+    it { should validate_numericality_of(:value).only_integer }
 
     describe 'uniqueness validation' do
       let(:user) { create(:user) }
+      let(:another_user) { create(:user) }
       let(:post) { create(:post, user: user) }
+      let(:another_post) { create(:post, user: another_user) }
+      let(:rating) { create(:rating, user: user, post: post, value: 4) }
+      let(:another_rating_same_user) { build(:rating, user: user, post: another_post, value: 5) }
+      let(:duplicate_rating) { build(:rating, user: user, post: post, value: 5) }
+      let(:another_rating) { build(:rating, user: another_user, post: post, value: 5) }
 
       it 'validates uniqueness of user_id scoped to post_id' do
-        create(:rating, user: user, post: post, value: 4)
-        duplicate_rating = build(:rating, user: user, post: post, value: 5)
-
+        rating
+        duplicate_rating
         expect(duplicate_rating).not_to be_valid
         expect(duplicate_rating.errors.messages[:user_id]).to include('A post can be rated once')
       end
 
       it 'allows the same user to rate different posts' do
-        another_post = create(:post, user: user)
-        create(:rating, user: user, post: post, value: 4)
-        second_rating = build(:rating, user: user, post: another_post, value: 5)
-
-        expect(second_rating).to be_valid
+        another_post
+        rating
+        expect(another_rating_same_user).to be_valid
       end
 
       it 'allows different users to rate the same post' do
-        another_user = create(:user)
-        create(:rating, user: user, post: post, value: 4)
-        second_rating = build(:rating, user: another_user, post: post, value: 5)
-
-        expect(second_rating).to be_valid
+        rating
+        expect(another_rating).to be_valid
       end
     end
   end
