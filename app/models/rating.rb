ac4b1455e22a4c72cd72f5c518f852046bb2f293
Joao Gilberto Saraiva <jgilbertons@gmail.com>
2025-04-26 09:46:13 -0300

    feat: create Rating model factory and test

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,4 +3,5 @@ class Rating < ApplicationRecord
   belongs_to :post
 
   validates :value, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  validates :user_id, uniqueness: { scope: :post_id, message: "A post can be rated once" }
 end
--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -9,5 +9,34 @@ RSpec.describe Rating, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:value) }
     it { should validate_numericality_of(:value).is_greater_than_or_equal_to(1).is_less_than_or_equal_to(5) }
+
+    describe 'uniqueness validation' do
+      let(:user) { create(:user) }
+      let(:post) { create(:post, user: user) }
+
+      it 'validates uniqueness of user_id scoped to post_id' do
+        create(:rating, user: user, post: post, value: 4)
+        duplicate_rating = build(:rating, user: user, post: post, value: 5)
+
+        expect(duplicate_rating).not_to be_valid
+        expect(duplicate_rating.errors.messages[:user_id]).to include('A post can be rated once')
+      end
+
+      it 'allows the same user to rate different posts' do
+        another_post = create(:post, user: user)
+        create(:rating, user: user, post: post, value: 4)
+        second_rating = build(:rating, user: user, post: another_post, value: 5)
+
+        expect(second_rating).to be_valid
+      end
+
+      it 'allows different users to rate the same post' do
+        another_user = create(:user)
+        create(:rating, user: user, post: post, value: 4)
+        second_rating = build(:rating, user: another_user, post: post, value: 5)
+
+        expect(second_rating).to be_valid
+      end
+    end
   end
 end
